2510b0a9e1aa9615ac43c033b690f1970cea2a01
soniaj05 <jsonia.infercon@gmail.com>
2024-12-13 19:10:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.use(express.json());
 
 // Database connection
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password:process.env.DB_PASSWORD,
-  database :process.env.DB_NAME
+  host: "localhost",
+  user: "root",
+  password:"$onia@24",
+  database :"student"
 });
 
 db.connect((err) => {
